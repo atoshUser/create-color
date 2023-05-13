72e2c97580d77d9eb8da0b36b2f10e5dbcef38e4
atoshUser <abdurahmonavezdjanov031@gmail.com>
2023-05-13 15:58:51 +0500

    add new algorithms

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const btn = document.querySelector("#btn");
 const input = document.querySelector("#input");
 const trash = document.querySelectorAll(".trash");
 const addNewItem = (value) => {
+    console.log(value);
     let elLI = document.createElement("li");
     let elTrash = document.createElement("span");
     let colorValue = document.createElement("span");
     colorValue.className = "value";
+    if (value.startsWith("url")) {
+        colorValue.style.fontSize = "15px";
+        colorValue.textContent = value;
+    }
     colorValue.textContent = value;
     colorValue.style.color = value;
     elLI.append(colorValue);
@@ -17,11 +22,24 @@ const addNewItem = (value) => {
     listItems.append(elLI);
 };
 
+let valueArr = [];
 btn.addEventListener("click", () => {
     let value = input.value;
-    input.value = "";
-    value = value.trim();
-    addNewItem(value);
+    if (valueArr.includes(value)) {
+        return false;
+    } else if (value.includes(".")) {
+        valueArr.push(input.value);
+        input.value = "";
+        value = `url(${value.trim()})`;
+        addNewItem(value);
+        console.log(valueArr);
+    } else {
+        valueArr.push(input.value);
+        input.value = "";
+        value = value.trim();
+        addNewItem(value);
+        console.log(valueArr);
+    }
 });
 
 listItems.addEventListener("click", (e) => {
